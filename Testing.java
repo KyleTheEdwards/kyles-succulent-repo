7837b818b065713f6819fe0d906cf350b21cda75
KyleTheEdwards <kedwards10@stuy.edu>
2019-02-26 22:28:45 -0500

    First Testing Iteration

---
+++ Testing.java
@@ -0,0 +1,24 @@
+import java.io.*;
+
+public class Testing{
+
+	public static void main(String[] args){
+
+		int times = 20;
+
+		System.out.println("Testing...\nHere are some fibonacci numbers:");
+
+		int[] nums = {0,1,0};
+
+		for(int x = 0; x < times; x++){
+			System.out.println(Integer.toString(nums[1]));
+			nums[2] = nums[0] + nums[1];
+			nums[0] = nums[1];
+			nums[1] = nums[2];
+
+		}
+
+
+	}
+
+}
