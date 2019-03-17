e9f6c701be8a58b2aa63df282d6e8b5da2e36b05
KyleTheEdwards <kedwards10@stuy.edu>
2019-03-17 16:51:26 -0400

    Very minor changes

--- 53_idiosyncraticMethods/Accumulator.java
+++ 53_idiosyncraticMethods/Accumulator.java
@@ -28,7 +28,7 @@ Accumulator.java:29: error: cannot find symbol
 /*
 A programmer should expect to be able to work around the stumbling block because the JVM knows the type of an element.
 
-Java's ____________ operator identifies the type of an element to the Java compiler 
+Java's instanceof operator identifies the type of an element to the Java compiler 
 */
 
 public class Accumulator {
@@ -42,6 +42,55 @@ public class Accumulator {
         List_inArraySlots list
       , String prefix
       ) {
+
+
+      /* 5.  Stumbling block 1
+      However, use of the operator alone is insufficient,
+      because the compiler (compiler? JVM?)
+      objects to the following code that adds use of
+      the operator to the code from Stumbling block 0:
+      */
+
+      // [code that illustrates the use of the operator]
+
+      /*
+      predicted error message:
+
+      actual error message:
+      */
+
+
+      /* 6. Workaround 1
+      Programmers use Java's _____________ operator
+      to tell the ___________ (compiler? JVM?)
+      that code uses a subclass's method on an object,
+      even though the reference to the object is stored
+      in a super-class variable.
+      */
+
+
+      /* 7. Stumbling block 2
+      However, use of this operator alone is insufficient,
+      because the ___________ (compiler? JVM?)
+      objects to the following code that adds use of
+      the operator to the code from Stumbling block 0:
+      */
+
+      // [code that illustrates the use of the operator]
+
+      /*
+      predicted error message:
+
+      actual error message:
+      */
+
+
+      /* 8. Workaround 2
+      A programmer can combine use of both operators
+      to apply a method to exactly those
+      elements in the list that support the method.
+      */
+
         String result = "";
         for(int element = 0; element < list.size(); element++){
           if(list.get(element) instanceof String){
@@ -49,7 +98,6 @@ public class Accumulator {
               result += list.get(element);
               result += ", ";
             }
-          }
         }
         return result;
     }
